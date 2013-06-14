269a2965bc089714616b1b213e37cd0a9811ec7a
Thomas Ituarte <thomas.ituarte@gmail.com>
2013-06-13 18:20:05 -0700

    Removing whitespace

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -12,7 +12,6 @@ describe ApplicationHelper do
       full_title('foo').should =~ /^Ruby on Rails Tutorial Sample App/
     end
 
-
     it "should not include a bar on the home page" do
       full_title('').should_not =~ /\|/
     end
